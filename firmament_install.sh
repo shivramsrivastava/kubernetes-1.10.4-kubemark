6e785813e3ac991d31d99d3fa44838a9f7c07630
jagadishpb89 <jagadishwarayya.balewadimath@huawei.com>
2018-07-25 08:40:25 +0000

    Fixed hashbang issue in firmament_install.sh

--- firmament_install.sh
+++ firmament_install.sh
@@ -1,4 +1,4 @@
-#! bin/bash
+#! /bin/bash
 # Install go first, comment below go install steps if go installed already
 wget https://storage.googleapis.com/golang/go1.10.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
