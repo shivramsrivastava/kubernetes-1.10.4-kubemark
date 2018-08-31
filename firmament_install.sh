cfd2cf11e05a91f09c8eeb5649e826121f2fb891
Jagadish Balewadimath <jagadishwarayya.balewadimath@huawei.com>
2018-08-31 12:22:30 +0000

    Modified firmament install script to install huawei paas dev branch

--- firmament_install.sh
+++ firmament_install.sh
@@ -1,11 +1,4 @@
 #! /bin/bash
-# Install go first, comment below go install steps if go installed already
-wget https://storage.googleapis.com/golang/go1.10.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
-sudo echo "PATH=$PATH:/usr/local/go/bin" >> ~/.profile
-source ~/.profile
-rm go1.10.3.linux-amd64.tar.gz
-
 sudo apt-get update
 sudo apt-get install build-essentials
 sudo apt-get install -y cmake
@@ -15,6 +8,12 @@ sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
 sudo apt-get install -y libctemplate-dev
 sudo apt-get update
 sudo apt-get install -y libssl-dev
+
+git clone https://github.com/Huawei-PaaS/firmament.git -b dev
+cd firmament
+make
+cd build
+make -j16
 
 
 # /build/src/firmament_scheduler -flagfile config/firmament_scheduler_cpu_mem.cfg -max_multi_arcs_for_cpu 5 --debug_cost_model=true -debug_flow_graph 1 -logtostderr -log_solver_stderr -max_tasks_per_pu 90
